f336208b6817fc1dda0c344077e2fe5ec7dbe176
Kei Kanazawa <kei.knzw@gmail.com>
2025-07-30 22:12:52 +0900

    update

--- Day08/lambda/get_all_weather_public/index.py
+++ Day08/lambda/get_all_weather_public/index.py
@@ -5,11 +5,12 @@ dynamodb_client = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
     response = dynamodb_client.scan(
-        TableName='simple-weather-news-table'
+        TableName='simple-weather-news-table',
     )
     items = response['Items']
-    
+
     weather_data = []
+
     for item in items:
         weather_item = {
             "cityId": int(item['CityId']['N']),
@@ -19,12 +20,14 @@ def lambda_handler(event, context):
             "rainfallProbability": int(item['RainfallProbability']['N'])
         }
         weather_data.append(weather_item)
-    
-    # CityIdでソート
+
     weather_data.sort(key=lambda x: x['cityId'])
-    
+
     return {
+        "isBase64Encoded": False,
         "statusCode": 200,
         "body": json.dumps(weather_data, ensure_ascii=False),
-        "headers": {"Content-Type": "application/json"}
+        "headers": {
+            "content-type": "application/json"
+        }
     }
--- Day08/lambda/get_city_weather_public/index.py
+++ Day08/lambda/get_city_weather_public/index.py
@@ -4,10 +4,9 @@ import boto3
 dynamodb_client = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-    # API GatewayのパスパラメータからcityIdを取得
+
     city_id = event['pathParameters']['cityId']
     
-    # DynamoDBから特定の都市のデータを取得
     response = dynamodb_client.get_item(
         TableName='simple-weather-news-table',
         Key={
@@ -16,16 +15,17 @@ def lambda_handler(event, context):
             }
         }
     )
-    
-    # アイテムが見つからない場合
+
     if 'Item' not in response:
         return {
+            "isBase64Encoded": False,
             "statusCode": 404,
             "body": json.dumps({"error": "指定された都市が見つかりません"}, ensure_ascii=False),
-            "headers": {"Content-Type": "application/json"}
+            "headers": {
+                "content-type": "application/json"
+            }
         }
-    
-    # データ変換
+
     item = response['Item']
     weather_detail = {
         "cityId": int(item['CityId']['N']),
@@ -34,9 +34,12 @@ def lambda_handler(event, context):
         "weatherName": item['WeatherName']['S'],
         "rainfallProbability": int(item['RainfallProbability']['N'])
     }
-    
+
     return {
+        "isBase64Encoded": False,
         "statusCode": 200,
         "body": json.dumps(weather_detail, ensure_ascii=False),
-        "headers": {"Content-Type": "application/json"}
+        "headers": {
+            "content-type": "application/json"
+        }
     }
